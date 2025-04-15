de9a76495d7995c569b9ac352e7ecbf30091b4ed
Azizul Islam <azizul.islam@technonext.com>
2025-04-15 10:32:49 +0600

    Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,40 @@
 package org.example
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    println(convertToRoundedString("88.50"))
+fun main() = runBlocking {
+    // Create a MutableSharedFlow that emits Int values
+    val sharedFlow = MutableSharedFlow<Int>()
 
+    // Collector 1
+    launch {
+        sharedFlow.collect { value ->
+            println("Collector 1 received: $value")
+        }
+    }
 
+    // Collector 2
+    launch {
+        sharedFlow.collect { value ->
+            println("Collector 2 received: $value")
+        }
+    }
+
+    // Emit some values from another coroutine
+    launch {
+        repeat(3) {
+            delay(500)
+            println("Emitting: $it")
+            sharedFlow.emit(it)
+        }
+    }
+
+    delay(3000) // Give enough time for everything to run
 }
-
-
 fun convertToRoundedString(value: String): String {
     return try {
         val doubleValue = value.toDouble() // Convert the string to a Double
